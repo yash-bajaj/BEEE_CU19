c74ee0770bf6d303488aa5bae0bf91f1ca1ecfd8
yash-bajaj <yashbajaj4444@gmail.com>
2019-09-20 01:24:05 +0530

    Add files via upload

---
+++ exquisite_inari1.ino
@@ -0,0 +1,14 @@
+void setup()
+{
+  pinMode(9, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(9, HIGH);
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
