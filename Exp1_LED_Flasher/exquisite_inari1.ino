f4b068904fe3b383ca03150972459c413ed67433
yash-bajaj <yashbajaj4444@gmail.com>
2019-09-20 01:48:04 +0530

    Update exquisite_inari1.ino

--- Exp1_LED_Flasher/exquisite_inari1.ino
+++ Exp1_LED_Flasher/exquisite_inari1.ino
@@ -7,7 +7,7 @@ void setup()
 void loop()
 {
   digitalWrite(9, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); 
   digitalWrite(9, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000);
 }
